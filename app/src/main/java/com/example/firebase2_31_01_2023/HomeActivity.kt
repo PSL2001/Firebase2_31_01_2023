822d1d1938572f17e64de837b4eb12dece891b1e
Pablo Sánchez López <65461723+PSL2001@users.noreply.github.com>
2023-02-03 09:16:01 +0100

    Añadido menu

--- app/src/main/java/com/example/firebase2_31_01_2023/HomeActivity.kt
+++ app/src/main/java/com/example/firebase2_31_01_2023/HomeActivity.kt
@@ -2,10 +2,37 @@ package com.example.firebase2_31_01_2023
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import com.example.firebase2_31_01_2023.databinding.ActivityHomeBinding
+import com.google.firebase.auth.FirebaseAuth
 
 class HomeActivity : AppCompatActivity() {
+    lateinit var binding: ActivityHomeBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)
+        binding = ActivityHomeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.home_menu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.item_logout -> {
+                FirebaseAuth.getInstance().signOut()
+                finish()
+                true
+            }
+            R.id.item_exit -> {
+                finishAffinity()
+                true
+            }
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
